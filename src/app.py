1d65ab1b8aebb2c56a26b01dc709a893d1cbc939
mauprogramador <sir.silvabmauricio@gmail.com>
2024-10-15 23:59:25 -0400

    feat: add exception/error handlers

--- src/app.py
+++ src/app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, redirect, render_template
 from flask_cors import CORS
+from werkzeug.exceptions import HTTPException
 
 
 app = Flask(
@@ -9,5 +10,20 @@ CORS(app)
 
 
 @app.get("/")
-def everything_is_okay():
+def everything_is_fine():
     return render_template("index.html")
+
+
+@app.errorhandler(HTTPException)
+def handle_http_exceptions(_):
+    return redirect("/")
+
+
+@app.errorhandler(400)
+def handle_bad_request(_):
+    return redirect("/")
+
+
+@app.errorhandler(404)
+def handle_not_found(_):
+    return redirect("/")
